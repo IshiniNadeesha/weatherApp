a6638a6675f41e407e4090688d521ed54e5e334e
IshiniNadeesha <ishini.nadeesha1@gmail.com>
2024-03-29 21:35:13 +0530

    new

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors()); // Use CORS middleware
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/weather_db', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/weather_db/weatherData', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
